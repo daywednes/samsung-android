e8498e38eab9e136db0dd77fa15f4d3239ae5ea0
Nguyen Son Tung <tung.nguyen@ncc.asia>
2020-07-20 20:33:25 +0700

    init android project

---
+++ android/app/src/main/java/one/harmony/puzzle/ui/game/AppActivity.java
@@ -0,0 +1,209 @@
+/****************************************************************************
+Copyright (c) 2015-2016 Chukong Technologies Inc.
+Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+http://www.cocos2d-x.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+package one.harmony.puzzle.ui.game;
+
+import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+import one.harmony.puzzle.contract.Puzzle;
+
+public class AppActivity extends Cocos2dxActivity {
+
+    private static final String TAG = AppActivity.class.getSimpleName();
+
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Workaround in https://stackoverflow.com/questions/16283079/re-launch-of-activity-on-home-button-but-only-the-first-time/16447508
+        if (!isTaskRoot()) {
+            // Android launched another instance of the root activity into an existing task
+            //  so just quietly finish and go away, dropping the user back into the activity
+            //  at the top of the stack (ie: the last state of this task)
+            // Don't need to finish it again since it's finished in super.onCreate .
+            return;
+        }
+        // DO OTHER INITIALIZATION BELOW
+        SDKWrapper.getInstance().init(this);
+
+        initParam();
+        testGetTopPlayers();
+
+    }
+
+    private void initParam() {
+        this.mCompositeDisposable = new CompositeDisposable();
+    }
+
+    public CompositeDisposable getCompositeDisposable() {
+        return mCompositeDisposable;
+    }
+
+    private void testGetTopPlayers() {
+        getCompositeDisposable().add(getTopPlayers()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<Void>() {
+
+                    @Override
+                    public void onNext(Void test) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                }));
+    }
+
+    public Observable<Void> getTopPlayers() {
+        return Observable.create(emitter -> {
+            Log.d(TAG, "getTopPlayers()");
+            String infuraUrl = "https://ropsten.infura.io/v3/3a84cb891979426aa702c7fea6090ef2";
+            Web3j web3j = Web3j.build(new HttpService(infuraUrl));
+            String puzzleContract = "0xCdCF4f95E8f2AA4710eD2D93474E4836183B9a08";
+            Credentials credentials = Credentials.create("155FD0A535250B64B9DF7956FC542A24343176B009D21C8111AFCE6AB74A5F96");
+
+            Puzzle puzzle = Puzzle.load(puzzleContract, web3j, credentials, new DefaultGasProvider());
+
+            try {
+                List topPlayers = puzzle.getTopPlayers().send();
+                Log.d(TAG, "getTopPlayers():topPlayers: " + topPlayers);
+            } catch (Exception e) {
+                Log.e(TAG, "getTopPlayers():" + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
+    
+    @Override
+    public Cocos2dxGLSurfaceView onCreateView() {
+        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);
+        // TestCpp should create stencil buffer
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);
+        SDKWrapper.getInstance().setGLSurfaceView(glSurfaceView, this);
+
+        return glSurfaceView;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SDKWrapper.getInstance().onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SDKWrapper.getInstance().onPause();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SDKWrapper.getInstance().onDestroy();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SDKWrapper.getInstance().onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        SDKWrapper.getInstance().onNewIntent(intent);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        SDKWrapper.getInstance().onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SDKWrapper.getInstance().onStop();
+    }
+        
+    @Override
+    public void onBackPressed() {
+        SDKWrapper.getInstance().onBackPressed();
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        SDKWrapper.getInstance().onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        SDKWrapper.getInstance().onRestoreInstanceState(savedInstanceState);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        SDKWrapper.getInstance().onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onStart() {
+        SDKWrapper.getInstance().onStart();
+        super.onStart();
+    }
+}
